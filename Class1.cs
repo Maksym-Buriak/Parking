f0aa9ab3b250cf41f24e898b1c487f3883467928
Maks <maksimburiakm@gmail.com>
2023-03-14 00:56:07 +0200

    Initial commit.

---
+++ Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Завдання_1
+{
+    internal class Parking
+    {
+        public int parking; //розмір парковки
+        public List<string> parkedCars; //парковка
+
+        public Parking(int parking)
+        {
+            this.parking = parking;
+            this.parkedCars = new List<string>(parking);
+        }
+
+        public void AddCar(string car) //додавання автомобілів до стоянки
+        {
+            if (parkedCars.Count < parking)
+            {
+                parkedCars.Add(car);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("Автомобіль ");
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(car);
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(" припарковано");
+                Console.ResetColor();
+                Console.WriteLine($"\nКількість автомобілів на стоянці: {parkedCars.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"Автомобіль {car} не припарковано (немає місць)");
+            }    
+        }
+
+        public void RemoveCar(string car) //видалення автомобілів зі стоянки
+        {
+            if (parkedCars.Contains(car))
+            {
+                parkedCars.Remove(car);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Автомобіль ");
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.Write(car);
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" видалено із стоянки");
+                Console.ResetColor();
+                Console.WriteLine($"\nКількість автомобілів на стоянці: {parkedCars.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"Автомобіль {car} не припаркований на цій стоянці");
+                Console.WriteLine("\nАвтомобілі з такими номерними знаками припарковані:");
+                foreach (string park in parkedCars)
+                {
+                    Console.WriteLine(park);
+                }
+            }
+        }
+    }
+}
+        
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Завдання_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int places = 3;
+            Parking lot = new Parking(places);
+            Console.WriteLine($"Кількість місць на стоянці: {places}");
+            try
+            {
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Виберіть дію: ");
+                    Console.ResetColor();
+                    Console.WriteLine("---------------------------");
+                    Console.WriteLine("Припаркувати автомобіль - 1");
+                    Console.WriteLine("Виїзд автомобіля - 2");
+                    Console.WriteLine("---------------------------");
+
+                    int choice = int.Parse(Console.ReadLine());
+                    if (choice != 1 && choice != 2 && Char.IsLetter(choice.ToString()[0]))
+                    {
+                        Console.WriteLine("Ви не правильно вибрали дію");
+                    }
+                    else if (choice == 1) // паркування автомобілів
+                    {
+                        bool continueParking = true;
+                        do
+                        {
+                            Console.Write("Введіть номер автомобіля у форматі |АА0000АА|: ");
+                            string carNumber = Console.ReadLine();
+                            if (lot.parkedCars.Contains(carNumber))
+                            {
+                                Console.WriteLine("Автомоіль з таким номером вже припаркований");
+                            }
+                            else if (carNumber.Length > 8 || carNumber.Length < 8)
+                            {
+                                Console.WriteLine("Ви неправильно ввели номер");
+                                Console.WriteLine("--------------------------");
+
+                                Console.WriteLine("\nБажаєте припаркувати автомобіль? Якщо так - написніть 1");
+                                Console.WriteLine("                                 Якщо ні  - натисніть Backspace");
+                                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                                if (keyInfo.Key == ConsoleKey.Backspace)
+                                {
+                                    continueParking = false; // зупиняємо роботу циклу після успішного додавання автомобіля
+                                }
+                                else
+                                {
+                                    continueParking = true; // продовжуємо роботу циклу
+                                }
+
+                            }
+                            else
+                            {
+                                lot.AddCar(carNumber);
+                                Console.WriteLine("\nБажаєте припаркувати автомобіль? Якщо так - написніть 1");
+                                Console.WriteLine("                                 Якщо ні  - натисніть Backspace");
+                                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                                if (keyInfo.Key == ConsoleKey.Backspace)
+                                {
+                                    continueParking = false; // зупиняємо роботу циклу після успішного додавання автомобіля
+                                }
+                                else
+                                {
+                                    continueParking = true; // продовжуємо роботу циклу
+                                }
+
+                            }
+                        }
+                        while (continueParking);
+                    }
+                    else if (choice == 2) // виїзд автомобілів з парковки
+                    {
+                        bool continueUnparking = true;
+                        do
+                        {
+                            Console.Write("Введіть номер автомобіля у форматі |АА0000АА|: ");
+                            string carNumber = Console.ReadLine();
+                            if (carNumber.Length > 8 || carNumber.Length < 8)
+                            {
+                                Console.WriteLine("Ви неправильно ввели номер");
+                                Console.WriteLine("--------------------------");
+                                Main(args);
+                            }
+                            else
+                            {
+                                lot.RemoveCar(carNumber);
+                                Console.WriteLine("\nБажаєте забрати автомобіль із стоянки? Якщо так - написніть 1");
+                                Console.WriteLine("                                       Якщо ні  - натисніть Backspace");
+                                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                                if (keyInfo.Key == ConsoleKey.Backspace)
+                                {
+                                    continueUnparking = false; // зупиняємо роботу циклу після успішного додавання автомобіля
+                                }
+                                else
+                                {
+                                    continueUnparking = true; // продовжуємо роботу циклу
+                                }
+                            }
+                        }
+                        while (continueUnparking);
+                    }
+                
+                }
+                while (true) ;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введений неправильний тип даних");
+            }
+        
+
+
+            Console.ReadKey();
+        }
+    }
+}
